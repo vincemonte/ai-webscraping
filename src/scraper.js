0b9dcf058b1581fbb86d271876a313089c33778f
vincemonte <vincentmonteleone99@gmail.com>
2024-04-04 14:13:46 -0400

    Implemented new functions for scraping using the gpt-4-vision-preview model. Also separated HTML scraping and screenshot scraping into different functions. Still a bug when writing responses from model to a new file. Will fix in next commit.

--- src/scraper.js
+++ src/scraper.js
@@ -8,8 +8,8 @@ const openai = new OpenAI({
 });
 const BD_AUTH=process.env.BD_AUTH;
 
-const TARGET_URL="https://www.kayak.com/hotels/Calgary,Alberta,Canada-p62667/2024-04-04/2024-04-05/2adults?sort=rank_a"
-async function scraper() {
+const TARGET_URL="https://www.google.com/travel/search?q=calgary&ts=CAEaHBIaEhQKBwjoDxAEGBESBwjoDxAEGBIYATICEAAqBwoFOgNVU0Q&ved=0CAAQ5JsGahcKEwjYvIKxjKmFAxUAAAAAHQAAAAAQew&ictx=3&qs=CAAgACgA&ap=MAA"
+async function scrape_html() {
     //Setting up the browser
     const browser = await puppeteer.connect({
         browserWSEndpoint: `wss://${BD_AUTH}@brd.superproxy.io:9222`
@@ -32,9 +32,26 @@ async function scraper() {
     await page.close();
 }
 
-async function call_openai(html) {
+async function scrape_screenshots(){
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: `wss://${BD_AUTH}@brd.superproxy.io:9222`
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    //Targeting URL and the specific content 
+    await page.goto(TARGET_URL, { waitUntil: "networkidle2" });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+    const currentDatetime = new Date().toISOString().replace(/[-:.]/g, "");
+    const screenshotPath = `screenshots/screenshot_${currentDatetime}.jpg`;
+    //Taking a screenshot 
+    const img64 = await page.screenshot({ path: screenshotPath, fullPage: true, encoding: "base64" });
+    return img64;
+}
+
+async function call_openai_html(html, model="gpt-3.5-turbo") {
     let response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: model,
         messages: [
             {
                 role: "system",
@@ -57,7 +74,39 @@ async function call_openai(html) {
 
 }
 
-async function main(){    
+async function call_openai_vision(encoding, model="gpt-4-vision-preview") {
+    const response = await openai.chat.completions.create({
+        model: "gpt-4-vision-preview",
+        messages: [
+          {
+            role: "user",
+            content: [
+              { type: "text", text: "Give me the hotel name, nightly rate, and the rating of each item in JSON Format. Don't include formatting or new lines." },
+              {
+                type: "image_url",
+                image_url: {
+                  "url": 'data:image/jpeg;base64,'+encoding,
+                },
+              },
+            ],
+          },
+        ],
+      });
+    const currentDatetime = new Date().toISOString().replace(/[-:.]/g, "");
+    console.log(response.choices[0]);
+    fs.writeFile(`responses/${currentDatetime}.txt`, response.choices[0], (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Response added to chatGPTResponse.txt");
+        }
+    });
+    return response.choices[0].message.content;
+
+}
+
+
+async function batch_html_requests(){    
     const html = fs.readFileSync("output.html", "utf8");
     const tokens = html.split(' '); // assuming space as a token separator
     const batches = [];
@@ -79,6 +128,11 @@ async function main(){
         const result = await call_openai(batches[i]);
         console.log(result);
     }
+}
+
+async function main() {
+    const img64=await scrape_screenshots();
+    await call_openai_vision(img64);
 }
 
 main().then(() => {
